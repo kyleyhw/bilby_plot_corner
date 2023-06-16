1a6b9c39fc9cc42373f59fe6c414b752a98bf67b
Kyle <kyleyhw@gmail.com>
2023-06-16 03:04:55 -0400

    ignores fixed parameters, fiddled with font sizes, positioned suptitle to top right

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from data_loader_json import DataLoaderJSON
 from plot_corner import PlotCorner
 
-# filename = 'bns_example_result'
-filename = 'bns_fixed_m_example_result'
+filename = 'bns_example_result'
+# filename = 'bns_fixed_m_example_result'
 
-parameters = ['mass_ratio', 'lambda_tilde', 'chi_eff']
+parameters = ['chirp_mass', 'mass_ratio', 'lambda_tilde', 'chi_eff']
 
 data = DataLoaderJSON(filename=filename + '.json', parameters=parameters)
 plotter = PlotCorner(data)
--- plot_corner.py
+++ plot_corner.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 class PlotCorner:
     def __init__(self, data):
         self.parameters = data.parameters
+        self.valid_parameters = []
         self.posteriors = []
         self.injection_values = []
         self.lower_quantiles = []
@@ -12,11 +13,15 @@ class PlotCorner:
         self.medians = []
 
         for parameter in self.parameters:
-            self.posteriors.append(data.posteriors_dict[parameter])
-            self.injection_values.append(data.injection_dict[parameter])
-            self.lower_quantiles.append(np.quantile(data.posteriors_dict[parameter], 0.05))
-            self.upper_quantiles.append(np.quantile(data.posteriors_dict[parameter], 0.95))
-            self.medians.append(np.median(data.posteriors_dict[parameter]))
+            if len(set(data.posteriors_dict[parameter])) == 1:
+                print('Ignoring fixed parameter ' + parameter + '.')
+            else:
+                self.valid_parameters.append(parameter)
+                self.posteriors.append(data.posteriors_dict[parameter])
+                self.injection_values.append(data.injection_dict[parameter])
+                self.lower_quantiles.append(np.quantile(data.posteriors_dict[parameter], 0.05))
+                self.upper_quantiles.append(np.quantile(data.posteriors_dict[parameter], 0.95))
+                self.medians.append(np.median(data.posteriors_dict[parameter]))
 
         self.posteriors = np.array(self.posteriors).T
         self.injection_values = np.array(self.injection_values)
@@ -25,18 +30,18 @@ class PlotCorner:
         self.medians = np.array(self.medians)
 
     def plot(self, show=False, save=False, title=''):
-        fig = corner.corner(data=self.posteriors, labels=self.parameters, show_titles=True, color='blue')
+        fig = corner.corner(data=self.posteriors, labels=self.valid_parameters, show_titles=True, color='blue', title_quantiles=[0.05, 0.5, 0.95], title_kwargs={'fontsize' : 11})
 
-        corner.overplot_lines(fig=fig, xs=self.injection_values, color='green')
-        corner.overplot_points(fig=fig, xs=self.injection_values[None], color='green')
+        corner.overplot_lines(fig=fig, xs=self.injection_values, color='orange')
+        corner.overplot_points(fig=fig, xs=self.injection_values[None], color='orange')
 
-        corner.overplot_lines(fig=fig, xs=self.medians, color='orange')
-        corner.overplot_points(fig=fig, xs=self.medians[None], color='orange')
+        corner.overplot_lines(fig=fig, xs=self.medians, color='black')
+        corner.overplot_points(fig=fig, xs=self.medians[None], color='black')
 
         corner.overplot_lines(fig=fig, xs=self.lower_quantiles, color='red')
         corner.overplot_lines(fig=fig, xs=self.upper_quantiles, color='red')
 
-        fig.suptitle(title)
+        fig.suptitle(title, fontsize=20, x=0.98, y=0.98, horizontalalignment='right')
 
         if save:
             plt.savefig(title + '_plot.png')
