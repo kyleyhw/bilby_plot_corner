dd87d1cbed6322fda38b4efae28080168f7e42a6
kyleyhw <kyleyhw@gmail.com>
2023-06-28 11:19:52 -0400

    added bns zero spin example

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ parameters = ['chirp_mass', 'mass_ratio', 'lambda_tilde', 'chi_eff']
 # data = DataLoaderJSON(filename='data/' + filename + '.json', parameters=parameters)
 
 # filename = 'bns_example_data0_1126259642-413_analysis_H1L1V1_result'
-filename = 'bns_free_spin_example_data0_1126259642-413_analysis_H1L1V1_result'
+# filename = 'bns_free_spin_example_data0_1126259642-413_analysis_H1L1V1_result'
+filename = 'bns_zero_spin_example_data0_1126259642-413_analysis_H1L1V1_result'
 data = DataLoaderH5(filename='data/' + filename + '.hdf5', parameters=parameters)
 
 plotter = PlotCorner(data)
