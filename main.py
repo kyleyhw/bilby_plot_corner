783518335950ce04180e57d94515fbee1062749b
kyleyhw <kyleyhw@gmail.com>
2023-07-04 00:12:15 -0400

    added search for L1L2 example

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ parameters = ['chirp_mass', 'mass_ratio', 'lambda_tilde', 'chi_eff']
 # filename = 'bns_fixed_m_example_result'
 # data = DataLoaderJSON(filename='data/' + filename + '.json', parameters=parameters)
 
-filename = 'bns_example_data0_1126259642-413_analysis_H1L1V1_result'
+# filename = 'bns_example_data0_1126259642-413_analysis_H1L1V1_result'
 # filename = 'bns_free_spin_example_data0_1126259642-413_analysis_H1L1V1_result'
 # filename = 'bns_zero_spin_example_data0_1126259642-413_analysis_H1L1V1_result'
+filename = 'bns_zero_spin_L1L2_example_data0_1126259642-413_analysis_H1L1V1_result'
 data = DataLoaderH5(filename='data/' + filename + '.hdf5', parameters=parameters)
 
 plotter = PlotCorner(data)
-plot_title = '_'.join(filename.split('_')[:2])
+plot_title = '_'.join(filename.split('_')[:5])
 print(plot_title)
 plotter.plot(show=False, save=True, title=plot_title)
