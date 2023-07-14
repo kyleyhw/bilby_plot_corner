04d86a536e6e6febd5ff8e447b9cbaab20182a58
kyleyhw <kyleyhw@gmail.com>
2023-07-14 03:13:41 -0400

    added binary love example plot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from data_loaders import DataLoaderJSON, DataLoaderH5
 from plot_corner import PlotCorner
 
-parameters = ['chirp_mass', 'mass_ratio', 'lambda_tilde', 'chi_eff']
+parameters = ['chi_eff', 'lambda_tilde', 'mass_ratio']
 
 
 # filename = 'bns_example_result'
@@ -11,10 +11,11 @@ parameters = ['chirp_mass', 'mass_ratio', 'lambda_tilde', 'chi_eff']
 # filename = 'bns_example_data0_1126259642-413_analysis_H1L1V1_result'
 # filename = 'bns_free_spin_example_data0_1126259642-413_analysis_H1L1V1_result'
 # filename = 'bns_zero_spin_example_data0_1126259642-413_analysis_H1L1V1_result'
-filename = 'bns_zero_spin_L1L2_example_data0_1126259642-413_analysis_H1L1V1_result'
+# filename = 'bns_zero_spin_L1L2_example_data0_1126259642-413_analysis_H1L1V1_result'
+filename = 'bns_binary_love_example_data0_1126259642-413_analysis_H1L1V1_result'
 data = DataLoaderH5(filename='data/' + filename + '.hdf5', parameters=parameters)
 
 plotter = PlotCorner(data)
-plot_title = '_'.join(filename.split('_')[:5])
+plot_title = '_'.join(filename.split('_')[:4])
 print(plot_title)
 plotter.plot(show=False, save=True, title=plot_title)
